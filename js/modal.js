a882077e627e20837e5fa881d7f13d23c689fb8f
Mariana <mariamna5204@gmail.com>
2022-01-07 13:59:49 +0200

    .

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('.main-title__btn'),
+    closeModalBtn: document.querySelector('.modal__close-btn'),
+    modal: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
